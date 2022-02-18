1d6d6e099e38f9a769b3b158fa440619b9464fb5
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-02-18 22:58:44 +0530

    added reconciliation service, aggregate, main class and changed signatures of csvhelper methods.

--- src/main/java/util/CSVHelper.java
+++ src/main/java/util/CSVHelper.java
@@ -3,23 +3,21 @@ package util;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
-import org.apache.commons.csv.CSVFormat;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.commons.csv.CSVFormat.Builder;
 import org.apache.commons.csv.CSVRecord;
 
 public class CSVHelper {
 
-  public static Stream<CSVRecord> readCsvStream(String fileName) {
+  public static List<CSVRecord> readCsvStream(String fileName) {
 
     try {
       Reader fileReader = new FileReader(fileName);
-      Iterable<CSVRecord> csvRecordIterable = CSVFormat.Builder.create()
-          .setSkipHeaderRecord(true)
-          .build()
-          .parse(fileReader);
-
-      return StreamSupport.stream(csvRecordIterable.spliterator(), true);
+      return Builder.create().setSkipHeaderRecord(true).build()
+          .parse(fileReader)
+          .stream()
+          .collect(Collectors.toList());
 
     } catch (IOException e) {
       throw new RuntimeException("Cannot parse: " + fileName);
