718c60ede668a437d59a5bd2cd2c635da83f2719
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-02-19 10:38:18 +0530

    added code to check value data type, created DataType.java convenience value class

--- src/main/java/FileReconciliationService.java
+++ src/main/java/FileReconciliationService.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Collections;
 import java.util.List;
 import org.apache.commons.csv.CSVRecord;
@@ -7,9 +10,12 @@ public class FileReconciliationService {
   private final List<CSVRecord> firstFile;
   private final List<CSVRecord> secondFile;
 
+  private List<DataType> dataTypes;
+
   public FileReconciliationService(List<CSVRecord> firstFile, List<CSVRecord> secondFile) {
     this.firstFile = firstFile;
     this.secondFile = secondFile;
+    populateDataTypes();
   }
 
   public ReconciliationAggregate reconcile() {
@@ -23,6 +29,20 @@ public class FileReconciliationService {
     return new ReconciliationAggregate();
   }
 
+  private void populateDataTypes() {
+    CSVRecord firstRecord = firstFile.get(0);
+    firstRecord.stream()
+        .map(value -> {
+          if (isValueDate(value)) {
+            return "date_time";
+          } else if (isValueNumber(value)) {
+            return "numeric";
+          }
+          return "string";
+        })
+        .forEach(dataType -> dataTypes.add(new DataType(dataType)));
+  }
+
   private List<CSVRecord> getExactMatches() {
 
 //    firstFile.stream().filter(csvRecord -> secondFile)
@@ -33,5 +53,24 @@ public class FileReconciliationService {
 
   private List<CSVRecord> getPartialMatches(List<CSVRecord> exactMatches) {
     return Collections.emptyList();
+  }
+
+  private boolean isValueDate(String value) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss:SSS");
+    try {
+      LocalDateTime.parse(value, formatter);
+      return true;
+    } catch (DateTimeParseException e) {
+      return false;
+    }
+  }
+
+  private boolean isValueNumber(String value) {
+    try {
+      Double.parseDouble(value);
+      return true;
+    } catch (NumberFormatException e) {
+      return false;
+    }
   }
 }
